f1c44ab743fbac11ef69faabb29c32629161802d
micky1608 <mteaudors@outlook.com>
2019-02-22 21:39:37 +0100

    AES inverse sbox

--- ISEC_TP2/isec_tp2.py
+++ ISEC_TP2/isec_tp2.py
@@ -76,7 +76,7 @@ def aes_sbox(alpha , m):
                     [0,0,0,1,1,1,1,1] ] )
 
 
-    Y = [1,1,0,0,0,1,1,0]
+    Y = [1, 1, 0, 0, 0, 1, 1, 0]
 
     res = np.array(((np.dot(A,list(reversed(inv_alpha))) + Y) % 2).T)
 
@@ -84,6 +84,39 @@ def aes_sbox(alpha , m):
 
     for i in range(len(res)):
         b += pow(2, i) if res[i] == 1 else 0
+
+    return b
+
+###################################################################################################################################
+
+def aes_sbox_inv(beta,m):
+
+    M = np.matrix([[0, 0, 1, 0, 0, 1, 0, 1],
+                   [1, 0, 0, 1, 0, 0, 1, 0],
+                   [0, 1, 0, 0, 1, 0, 0, 1],
+                   [1, 0, 1, 0, 0, 1, 0, 0],
+                   [0, 1, 0, 1, 0, 0, 1, 0],
+                   [0, 0, 1, 0, 1, 0, 0, 1],
+                   [1, 0, 0, 1, 0, 1, 0, 0],
+                   [0, 1, 0, 0, 1, 0, 1, 0]])
+
+    Y = [1,0,1,0,0,0,0,0]
+
+    beta = beta.coef
+
+    if (len(beta) < 8):
+        beta = np.hstack((np.zeros(8 - len(beta)), beta))
+
+    beta = list(reversed(beta))
+
+    temp = np.poly1d(list(reversed(np.squeeze(np.asarray((np.dot(M,beta) + Y) % 2)))))
+
+    inv = inv_mod(temp,m)
+
+    b = 0
+
+    for i in range(len(inv)):
+        b += pow(2, len(inv)-1-i) if inv[i] == 1 else 0
 
     return b
 
@@ -111,14 +144,22 @@ def show_hexa_table(table):
 
 ###################################################################################################################################
 
+
+
+
 alpha = np.poly1d([1,1,0,1,1,0,0,1])
 m = np.poly1d([1,0,0,0,1,1,0,1,1])
 
 AES_table = np.zeros((16,16))
+AES_inv_table = np.zeros((16,16))
 
 for i in range(0x10):
     for j in range(0x10):
         AES_table[i][j] = aes_sbox(pol_from_hexa(i,j) , m)
+        AES_inv_table[i][j] = aes_sbox_inv(pol_from_hexa(i, j), m)
+
 
 show_hexa_table(AES_table)
+print("\n**********************************************************************************************\n")
+show_hexa_table(AES_inv_table)
 
